043c05edc4db7cd83632d2b6d051d5c9eeec96ee
joe cutter <cjnjenga@gmail.com>
2018-10-13 17:17:03 +0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 13, 2018 at 04:01 PM
+-- Generation Time: Oct 13, 2018 at 04:16 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.8
 
@@ -23,38 +23,11 @@ SET time_zone = "+00:00";
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
   `product` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
+  `price` varchar(20) NOT NULL,
   `pending` tinyint(1) NOT NULL DEFAULT '1',
   `quantity` int(11) NOT NULL,
   `color` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
-
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
-  `product` varchar(50) NOT NULL,
-  `price` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`id`, `product`, `price`) VALUES
-(1, 'wine1', 'ksh100'),
-(2, 'wine2', 'ksh200'),
-(3, 'wine3', 'ksh300'),
-(4, 'wine4', 'ksh100'),
-(5, 'wine5', 'ksh1200'),
-(6, 'wine6', 'ksh1000'),
-(7, 'wine7', 'ksh500'),
-(8, 'wine8', 'ksh600'),
-(9, 'wine9', 'ksh800');
 
 --
 -- Indexes for dumped tables
@@ -67,12 +40,6 @@ ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -81,8 +48,3 @@ ALTER TABLE `products`
 --
 ALTER TABLE `orders`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
